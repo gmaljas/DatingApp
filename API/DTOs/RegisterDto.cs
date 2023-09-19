c0e0bd86b78b05e36d64ff188f53183f78a7973d
Gabriel Maljas <g.maljas@vmt-gmbh.de>
2023-09-19 14:20:36 +0200

    End of Section 4

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
