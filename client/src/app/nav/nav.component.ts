d21399f4844ab6fe0643a13c4b20682b1911b0ca
Gabriel Maljas <g.maljas@vmt-gmbh.de>
2023-09-26 15:44:05 +0200

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -18,13 +18,13 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: _ => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
+      next: _ => this.router.navigateByUrl('/members')
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 
 }
